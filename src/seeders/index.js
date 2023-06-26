83f93645e5bd6e19aca44005ebaebfe5b1e9d649
phuongvo <phuong.vv@techchain.vn>
2023-06-26 21:11:06 +0700

    feat: add function seeder data address

--- src/seeders/index.js
+++ src/seeders/index.js
@@ -1,9 +1,13 @@
+const seedDistrict = require('./district');
 const seedMod = require('./moderator');
 const seedProvince = require('./province');
+const seedWard = require('./ward');
 
 const runSeeder = async () => {
   await seedMod().catch(err => console.log(err));
   await seedProvince().catch(err => console.log(err))
+  await seedDistrict().catch(err => console.log(err));
+  await seedWard().catch(err => console.log(err));
   console.log('Seed data successfully.');
 
 };
